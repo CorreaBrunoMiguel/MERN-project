66345deb63dcdde10f9a69a58628798fa62c38c4
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-04-02 15:51:01 -0300

    complete sign up page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-
 export default function SignIn() {
-  return <div>Chupa</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+    </div>
+  );
 }
